5640b5498a2868c6fcc186f58995e13a674c066f
JOÃO LUCAS GOMES DE SOUZA <20221101110040@ifrn.local>
2025-10-07 14:15:25 -0300

    Upt: Responsive navbar

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,36 +1,44 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { fas } from "@fortawesome/free-solid-svg-icons";
-import { NavLink } from "react-router-dom"; // ⬅️ troque Link por NavLink
+import { NavLink } from "react-router-dom";
 import "./Navbar.css";
 
 export default function Navbar() {
     return (
         <nav className="navbar">
-            <div className="navbarLogo"><img src="./img/LogoSVG.svg" alt="" />LevelLearn</div>
+            <div className="navbarLogo">
+                <img src="./img/LogoSVG.svg" alt="Logo" />
+                <span>LevelLearn</span>
+            </div>
             <ul className="navbarLinks">
                 <li>
                     <NavLink to="/" end>
-                        <FontAwesomeIcon size="lg" icon={fas.faHouse}/> PÁGINA INICIAL
+                        <FontAwesomeIcon size="lg" icon={fas.faHouse} />
+                        <span>PÁGINA INICIAL</span>
                     </NavLink>
                 </li>
                 <li>
                     <NavLink to="/study">
-                        <FontAwesomeIcon size="lg" icon={fas.faBook}/> ESTUDAR
+                        <FontAwesomeIcon size="lg" icon={fas.faBook} />
+                        <span>ESTUDAR</span>
                     </NavLink>
                 </li>
                 <li>
                     <NavLink to="/friends">
-                        <FontAwesomeIcon size="lg" icon={fas.faUserGroup}/> AMIGOS
+                        <FontAwesomeIcon size="lg" icon={fas.faUserGroup} />
+                        <span>AMIGOS</span>
                     </NavLink>
                 </li>
                 <li>
                     <NavLink to="/store">
-                        <FontAwesomeIcon size="lg" icon={fas.faStore}/> LOJA
+                        <FontAwesomeIcon size="lg" icon={fas.faStore} />
+                        <span>LOJA</span>
                     </NavLink>
                 </li>
                 <li>
                     <NavLink to="/profile">
-                        <FontAwesomeIcon size="lg" icon={fas.faUser}/> MEU PERFIL
+                        <FontAwesomeIcon size="lg" icon={fas.faUser} />
+                        <span>MEU PERFIL</span>
                     </NavLink>
                 </li>
             </ul>
