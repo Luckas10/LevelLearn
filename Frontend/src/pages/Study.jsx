9719070e37deb24a2bcaaab032a2cc171348ddbb
Mateus Batista Almeida <20221101110064@ifrn.local>
2025-10-10 17:56:55 -0300

    Add: Study base html

--- Frontend/src/pages/Study.jsx
+++ Frontend/src/pages/Study.jsx
@@ -1,4 +1,6 @@
 import Navbar from "../components/Navbar";
+import flashcardsimg from "../assets/Flashcards-img.png";
+import pomodoreimg from "../assets/Pomodore-img.png"
 import "./Study.css";
 
 export function Study() {
@@ -6,8 +8,18 @@ export function Study() {
         <div className="study-page">
             <Navbar />
             <section>
-                <h1>Study Page</h1>
-                <p>This is where users can access study materials and resources.</p>
+                <div className="flashcards-container">
+                    <div id="flashcards-img"><img src={flashcardsimg} alt="Flashcards Image" /></div>
+                    <div>
+                        <h1>FLASH CARDS</h1>
+                    </div>
+                </div>
+                <div className="pomodore-container">
+                    <div id="pomodore-img"><img src={pomodoreimg} alt="Pomodore Image" /></div>
+                    <div>
+                        <h1>POMODORO</h1>
+                    </div>
+                </div>
             </section>
         </div>
     );
