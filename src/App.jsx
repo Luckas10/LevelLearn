670ffa2d392d7cb509955f9a141e4d1d8ac37ae2
André Medeiros Silva <andre1998vv@gmail.com>
2025-10-12 18:24:47 -0300

    Add: PublicRoute component

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,22 @@ import "./App.css"
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import ProtectedRoute from "./components/ProtectedRoute";
+import PublicRoute from "./components/PublicRoute";
 import Register from "./pages/Register";
+
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login"
+          element={
+            <PublicRoute>
+              <Login />
+            </PublicRoute>
+          }
+        />
         <Route
           path="/dashboard"
           element={
