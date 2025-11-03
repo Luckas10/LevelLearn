e5b32a6d6a447b11bbe1018810f9f8c4bb527232
Teus36 <mateus_batistaalmeida@hotmail.com>
2025-11-02 23:09:42 -0300

    add create collection modal

--- Frontend/src/components/StudyFlashCards/StudyCreateCollection.jsx
+++ Frontend/src/components/StudyFlashCards/StudyCreateCollection.jsx
@@ -1,37 +1,61 @@
+import { useEffect, useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 export default function StudyCreateCollection() {
-    
+    const dialogRef = useRef(null);
 
-    return (
-        <div className="studyCreateCollection">
-            <button className="createCollection-btn">
-                <FontAwesomeIcon size="lg" icon={faPlus} />
-                <p>CRIAR COLEÇÃO</p>
-            </button>
+    useEffect(() => {
+        const dialog = dialogRef.current;
 
-            <dialog >
-                <div>
-                    <h1>CRIAR COLEÇÃO</h1>
-                    <input type="text" />
-                    <input type="text" />
-                    <div>
-                        <h1>CAPA DA COLEÇÃO</h1>
-                        <div>
-                            <div className=""></div>
-                            <div className=""></div>
-                            <div className=""></div>
-                            <div className=""></div>
-                            <div className=""></div>
-                            <div className=""></div>
-                        </div>
-                    </div>
-                    <button></button>
-                </div>
-            </dialog>
+        // Fecha ao clicar fora do conteúdo
+        const handleClickOutside = (event) => {
+        const dialogDimensions = dialog.getBoundingClientRect();
+        if (
+            event.clientX < dialogDimensions.left ||
+            event.clientX > dialogDimensions.right ||
+            event.clientY < dialogDimensions.top ||
+            event.clientY > dialogDimensions.bottom
+        ) {
+            dialog.close();
+        }
+        };
 
+        dialog.addEventListener("click", handleClickOutside);
+
+        return () => dialog.removeEventListener("click", handleClickOutside);
+    }, []);
+
+    const openModal = () => {
+        dialogRef.current.showModal();
+    };
+
+  return (
+    <div className="studyCreateCollection">
+      <button className="createCollectionButton" onClick={openModal}>
+        <FontAwesomeIcon size="lg" icon={faPlus} />
+        <p>CRIAR COLEÇÃO</p>
+      </button>
+
+      <dialog className="createCollectionModal" ref={dialogRef}>
+        <div className="collectionModalContent">
+          <p>CRIAR COLEÇÃO</p>
+          <input className="collectionInput" type="text" placeholder="Nome: " />
+          <input className="collectionInput" type="text" placeholder="Descrição: " />
+          <div className="collectionCoverSelect">
+            <p>CAPA DA COLEÇÃO</p>
+            <div>
+              <div className="collectionCover"></div>
+              <div className="collectionCover"></div>
+              <div className="collectionCover"></div>
+              <div className="collectionCover"></div>
+              <div className="collectionCover"></div>
+              <div className="collectionCover"></div>
+            </div>
+          </div>
+          <button className="collectionButtonSave">SALVAR</button>
         </div>
-    )
+      </dialog>
+    </div>
+  );
 }
-
