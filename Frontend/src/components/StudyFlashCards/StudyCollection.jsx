c895be0a82ef2c75a08824227580ba7dfce0dacf
Mateus Batista Almeida <20221101110064@ifrn.local>
2025-11-19 14:05:18 -0300

    Adjustments: CoverImages path

--- Frontend/src/components/StudyFlashCards/StudyCollection.jsx
+++ Frontend/src/components/StudyFlashCards/StudyCollection.jsx
@@ -1,6 +1,6 @@
 import StudyCreateCollection from "./StudyCreateCollection";
 import StudyInteractiveCard from "./StudyInteractiveCard";
-import flashcardsimg from "../../assets/CoverImages/fisica.svg";
+import flashcardsimg from "/CoverImages/fisica.svg";
 import { NavLink } from "react-router-dom";
 
 export default function StudyCollection() {
