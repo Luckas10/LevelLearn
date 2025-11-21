a2576dfe731d2cfb63bdd5666cc61bb322f99caa
JOÃO LUCAS GOMES DE SOUZA <20221101110040@ifrn.local>
2025-11-21 16:34:28 -0300

    Add

---
+++ Frontend/src/components/StudyFlashCards/StudyLibrary.jsx
@@ -0,0 +1,14 @@
+import StudyCreateCollection from "./StudyCreateCollection";
+import StudyInteractiveCard from "./StudyInteractiveCard";
+import flashcardsimg from "../../assets/CoverImages/fisica.svg";
+import { NavLink } from "react-router-dom";
+
+export default function StudyLibrary() {
+
+    return (
+        <div className="studyCollection-page">
+            
+        </div>
+    );
+}
+
