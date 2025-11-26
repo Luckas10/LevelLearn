48eaf066869cbd58fd6b6354ba86c1f86931c77f
Teus36 <mateus_batistaalmeida@hotmail.com>
2025-11-26 11:55:54 -0300

    Adjustments: Enviando o backend que eu esqueci

--- BackEnd/routers/decks.py
+++ BackEnd/routers/decks.py
@@ -1,26 +1,31 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, HTTPException
 from sqlmodel import select
 from typing import List
-from models import Deck, User
+from models import Deck
 from database import SessionDep
-
-from routers.auth import get_current_user
 
 router = APIRouter(prefix="/decks", tags=["Decks"])
 
 @router.get("")
-def listar_decks(session: SessionDep, current_user: User = Depends(get_current_user)) -> List[Deck]:
+def listar_decks(session: SessionDep) -> List[Deck]:
     return session.exec(select(Deck)).all()
 
 @router.post("")
-def cadastrar_deck(session: SessionDep, deck: Deck, current_user: User = Depends(get_current_user)) -> Deck:
+def cadastrar_deck(session: SessionDep, deck: Deck) -> Deck:
     session.add(deck)
     session.commit()
     session.refresh(deck)
     return deck
 
+@router.get("/{id}")
+def obter_deck(session: SessionDep, id: int) -> Deck:
+    deck = session.get(Deck, id)
+    if not deck:
+        raise HTTPException(status_code=404, detail="Deck não encontrado")
+    return deck
+
 @router.delete("/{id}")
-def deletar_deck(session: SessionDep, id: int, current_user: User = Depends(get_current_user)) -> str:
+def deletar_deck(session: SessionDep, id: int) -> str:
     deck = session.exec(select(Deck).where(Deck.id == id)).one()
     session.delete(deck)
     session.commit()
