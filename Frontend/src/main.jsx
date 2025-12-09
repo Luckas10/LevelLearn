a430cf3e43b1339a400e89a314e56d6669031c85
André Medeiros Silva <andre1998vv@gmail.com>
2025-12-09 17:38:01 -0300

    Fixed: lots of data-theme class errors

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,8 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./style.css";
-import "./Swal.css"
-import "./LightMode.css"
+import "./Swal.css";
+import "./LightMode.css";
+
+const savedTheme =
+  typeof window !== "undefined" ? localStorage.getItem("theme") : null;
+
+if (savedTheme === "dark" || savedTheme === "light") {
+  document.documentElement.setAttribute("data-theme", savedTheme);
+} else {
+  document.documentElement.setAttribute("data-theme", "dark");
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
